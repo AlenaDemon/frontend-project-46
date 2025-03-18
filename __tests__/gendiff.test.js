e5c4b69f311d58cef8ec588fba7cc7cc1ab19ce1
AlenaDemon <drmkpr@mail.ru>
2025-03-18 20:42:46 +0400

    test

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,16 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import gendiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+test('gendiff', () => {
+  expect(gendiff(
+    getFixturePath('file1.json'),
+    getFixturePath('file2.json'),
+  )).toBe(readFile('result.txt'));
+});
